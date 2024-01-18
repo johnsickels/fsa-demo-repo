a85912a7ee077b2b1960ece2ff9ad9143a20be68
John Sickels <johnwsickels@gmail.com>
2024-01-17 22:36:42 -0500

    change var to red

--- code04.js
+++ code04.js
@@ -2,7 +2,7 @@ console.log("Hello Fullstack!")
 console.log("Wait here")
 console.log("Goodbye Fullstack!")
 
-var blue = 'dabadee dabadie';
+var red = 'dabadee dabadie';
 
 function sayHi() {
     console.log('whaaat is happening')
