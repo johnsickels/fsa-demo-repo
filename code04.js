5ab5d6acb6074b46d06b8821886dd5a0ae3d1c41
John Sickels <johnwsickels@gmail.com>
2024-01-17 22:14:15 -0500

    change var name from a to b

--- code04.js
+++ code04.js
@@ -2,7 +2,7 @@ console.log("Hello Fullstack!")
 console.log("Wait here")
 console.log("Goodbye Fullstack!")
 
-var a = 'foobar';
+var b = 'foobar';
 
 function sayHi() {
     console.log('whaaat is happening')
