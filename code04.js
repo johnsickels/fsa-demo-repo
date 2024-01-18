9460cf8bbec762a84cf5651e6807197f5af80e90
John Sickels <johnwsickels@gmail.com>
2024-01-17 22:23:11 -0500

    refactor variable

--- code04.js
+++ code04.js
@@ -2,7 +2,7 @@ console.log("Hello Fullstack!")
 console.log("Wait here")
 console.log("Goodbye Fullstack!")
 
-var c = 'foobar';
+var blue = 'dabadee dabadie';
 
 function sayHi() {
     console.log('whaaat is happening')
